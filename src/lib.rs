339e15f4b84fc8d4ac4fd02ec916225cbd228bb5
Emil Hammarstrom <emil.hammarstrom@tedsys.se>
2019-10-02 15:19:34 +0200

    Added attribute Subscriber parameter

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
 #[cfg(test)]
 mod tests {
-    use tracing::{event, span, Level, instrument, subscriber};
-    use tracing_subscriber::{FmtSubscriber};
-    use tracing_proc_macros::{traced_test};
+    use tracing::{event, instrument, span, subscriber, Level};
+    use tracing_proc_macros::traced_test;
+    use tracing_subscriber::FmtSubscriber;
 
     #[test]
     #[instrument]
     fn test1() {
         let fmt_subscriber = FmtSubscriber::new();
         subscriber::with_default(fmt_subscriber, || {
-
             event!(Level::INFO, "T1: INFO 1");
             let span = span!(Level::INFO, "my_span");
             let _guard = span.enter();
@@ -17,11 +16,10 @@ mod tests {
             drop(_guard);
             event!(Level::DEBUG, "T1: DEBUG 2");
             event!(Level::INFO, "T1: INFO 2");
-
         });
     }
 
-    #[traced_test]
+    #[traced_test(FmtSubscriber)]
     fn test2() {
         event!(Level::INFO, "T2: INFO 1");
         let span = span!(Level::INFO, "my_span");
